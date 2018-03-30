6e8029b94475996c51003661180f41a968430a42
Mattias Persson <mattias@happypixels.se>
2018-03-30 20:14:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # vue-axios-interceptors
-Catch and handle ajax errors globally
+Catch and handle ajax errors globally when using `Axios` with `Vue`.
+
+### Installation
+```bash
+npm install vue-axios-interceptors --save
+// or
+yarn add url-parameters
+```
+
+```javascript
+// Make sure you import this package after you've imported Vue:
+window.Vue = require('vue');
+
+require('vue-axios-interceptors');
+
+// Make sure the axios package is available globally on the window object:
+window.axios = require('axios');
+```
+
+### Usage
+The package registers a new global event bus called `intercepted` on the `window` object and emits several events on it when an ajax call leads to an error. You can easily listen for these events to build a smooth error handling workflow, for example in a global component responsible for displaying error messages:
+
+```javascript
+// ErrorMessages.vue
+mounted() {
+    window.intercepted.$on('response', data => {
+        console.log(data); // { status: 404, code: 'Not found', body: null }
+        
+        // Display the message.
+    });
+}
+```
+
+You can also listen for specific status codes and response categories, for example if you'd like to handle 4xx responses differently than 5xx responses:
+```javascript
+// Listen for any intercepted responses.
+window.intercepted.$on('response', data => {
+  // 
+});
+
+// Listen for any intercepted responses under the Client Error category (4xx).
+window.intercepted.$on('response:client-error', data => {
+  // 
+});
+
+// Listen for any intercepted responses under the Server Error category (5xx).
+window.intercepted.$on('response:5xx', data => {
+  // 
+});
+
+// Listen for a specific status.
+window.intercepted.$on('response:404', data => {
+  // 
+});
+
+// Listen for a specific HTTP code.
+window.intercepted.$on('response:unprocessable-entity', data => {
+  // 
+});
+```
+
+For a complete list of status codes, visit https://httpstatuses.com/.
+
+### Using this package with Laravel
+If you're using Laravel >=5.5 as your backend, you're in luck. If your server returns a `422` response (typically a validation error), the package will automatically parse the returned failures into a flat array which you can access on `data.body`. This is way simpler to iterate through in order to display the messages than the original error message structure.
---
+++ intercept.js
@@ -0,0 +1,136 @@
+try {
+    window.intercepted = new Vue();
+} catch (e){
+    console.error('Require vue-axios-interceptors after you require Vue.');
+}
+
+window.axios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    var categories      = ['informational', 'success', 'redirection', 'client-error', 'server-error'];
+    var status          = error.response.status;
+    var codes           = statusCodes();
+
+    if(!codes[status]){
+        return Promise.reject(error);
+    }
+
+    var statusCategory  = parseInt(status.toString().charAt(0));
+    var category        = categories[statusCategory - 1];
+    var sluggedCode     = slugify(codes[status]);
+    var data            = {status: status, code: codes[status], body: null};
+
+    // Parse the validation errors.
+    if(parseInt(status) === 422){
+        data.body = handleValidationErrors(error);
+    }
+
+    window.intercepted.$emit('response', data);
+    window.intercepted.$emit('response:' + category, data);
+    window.intercepted.$emit('response:' + sluggedCode, data);
+    window.intercepted.$emit('response:' + status, data);
+    window.intercepted.$emit('response:' + statusCategory + 'xx', data);
+    
+    return Promise.reject(error);
+});
+
+function handleValidationErrors(error){
+    if(!error.response.data){
+        return null;
+    }
+
+    // Attempt to parse Laravel-structured validation errors.
+    try {
+        let messages = [];
+      
+        for(var key in error.response.data.errors){
+            error.response.data.errors[key].map(function(message){
+                messages.push(message);
+            });
+        }
+
+        return messages;
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+function slugify(string){
+    return string.toString().toLowerCase().trim()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/&/g, '-and-')         // Replace & with 'and'
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-'); 
+}
+
+function statusCodes(){
+    return {
+        '100': 'Continue',
+        '101': 'Switching Protocols',
+        '102': 'Processing',
+
+        '200': 'OK',
+        '201': 'Created',
+        '202': 'Accepted',
+        '203': 'Non-authoritative Information',
+        '204': 'No Content',
+        '205': 'Reset Content',
+        '206': 'Partial Content',
+        '207': 'Multi-Status',
+        '208': 'Already Reported',
+        '226': 'IM Used',
+
+        '300': 'Multiple Choices',
+        '301': 'Moved Permanently',
+        '302': 'Found',
+        '303': 'See Other',
+        '304': 'Not Modified',
+        '305': 'Use Proxy',
+        '307': 'Temporary Redirect',
+        '308': 'Permanent Redirect',
+
+        '400': 'Bad Request',
+        '401': 'Unauthorized',
+        '402': 'Payment Required',
+        '403': 'Forbidden',
+        '404': 'Not Found',
+        '405': 'Method Not Allowed',
+        '406': 'Not Acceptable',
+        '407': 'Proxy Authentication Required',
+        '408': 'Request Timeout',
+        '409': 'Conflict',
+        '410': 'Gone',
+        '411': 'Length Required',
+        '412': 'Precondition Failed',
+        '413': 'Payload Too Large',
+        '414': 'Request-URI Too Long',
+        '415': 'Unsupported Media Type',
+        '416': 'Requested Range Not Satisfiable',
+        '417': 'Expectation Failed',
+        '418': 'I\'m a teapot',
+        '421': 'Misdirected Request',
+        '422': 'Unprocessable Entity',
+        '423': 'Locked',
+        '424': 'Failed Dependency',
+        '426': 'Upgrade Required',
+        '428': 'Precondition Required',
+        '429': 'Too Many Requests',
+        '431': 'Request Header Fields Too Large',
+        '444': 'Connection Closed Without Response',
+        '451': 'Unavailable For Legal Reasons',
+        '499': 'Client Closed Request',
+
+        '500': 'Internal Server Error',
+        '501': 'Not Implemented',
+        '502': 'Bad Gateway',
+        '503': 'Service Unavailable',
+        '504': 'Gateway Timeout',
+        '505': 'HTTP Version Not Supported',
+        '506': 'Variant Also Negotiates',
+        '507': 'Insufficient Storage',
+        '508': 'Loop Detected',
+        '510': 'Not Extended',
+        '511': 'Network Authentication Required',
+        '599': 'Network Connect Timeout Error',
+    };
+}
